86c10c986d4724d16f386c83bb66054e1a75432b
chx <18768113677@163.com>
2015-12-29 19:20:13 +0800

    init

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib import admin
+# Create your models here.
+class BlogsPost(models.Model):
+	title = models.CharField(max_length=150)
+	body = models.TextField()
+	timestamp = models.DateTimeField()
+
+class BlogsPostAdmin(admin.ModelAdmin):
+	list_display=('title','timestamp')
+
+admin.site.register(BlogsPost)
